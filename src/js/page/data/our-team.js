ce373c77df1aeb1e2b5d8ffaa97cc5923c90d91b
Veronika2007931 <113263850+Veronika2007931@users.noreply.github.com>
2023-05-06 12:58:15 +0300

    додає елементи

--- src/js/page/data/our-team.js
+++ src/js/page/data/our-team.js
@@ -1,8 +1,61 @@
-const team = [
-    {
-        id,
-        name,
-        discription,
-        img
-    }
-]
+// const team = [
+//     {
+//         id,
+//         name,
+//         discription,
+//         img
+//     },
+//     {
+//         id,
+//         name,
+//         discription,
+//         img
+//     },
+//     {
+//         id,
+//         name,
+//         discription,
+//         img
+//     },
+//     {
+//         id,
+//         name,
+//         discription,
+//         img
+//     },
+//     {
+//         id,
+//         name,
+//         discription,
+//         img
+//     },
+//     {
+//         id,
+//         name,
+//         discription,
+//         img
+//     }
+// ]
+
+const divEl = document.querySelector('.first')
+
+const titleEl = document.createElement('h1')
+titleEl.textContent = 'Наша команда'
+divEl.append(titleEl)
+
+const photo = document.createElement('img')
+photo.src = team[0].img
+divEl.append(photo) 
+
+const namesEl = document.createElement('p')
+namesEl.textContent = ''
+divEl.append(namesEl)
+
+const doWork = document.createElement('p')
+doWork.textContent = ''
+divEl.append(doWork)
+
+
+
+
+
