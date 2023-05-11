b84c0ff1b2922f4c12541138e6579e2e14083ee5
Maria89698 <maria9parhomova@gmail.com>
2023-05-11 20:09:13 +0200

    add max number game

---
+++ src/js/page/games/max-number.js
@@ -0,0 +1,65 @@
+const section = document.createElement("section");
+section.classList.add('section');
+
+const box = document.getElementById("8");
+box.classList.add('container');
+// section.appendChild(box);
+
+
+// heading
+const heading = document.createElement ("h2");
+heading.textContent = "Введіть 3 числа";
+box.appendChild(heading);
+
+let maxValue = 0;
+
+const inputList = document.createElement("ul");
+box.appendChild(inputList);
+
+const answer = document.createElement("p");
+box.appendChild(answer);
+answer.textContent = "Найбільше число, яке ви ввели - ";
+
+let inputElements = [];
+let inputValues = [];
+
+
+for (let i = 0; i < 3; i += 1){
+    inputElements[i] = document.createElement("li");
+    inputList.appendChild(inputElements[i]);
+
+    inputValues[i] = document.createElement("input");
+
+    inputValues[i].type = 'number';
+    inputValues[i].placeholder = "Введіть число";
+    inputElements[i].appendChild(inputValues[i]);
+
+    inputValues[i].addEventListener("change", (event) => {
+        maxValue = Math.max(maxValue, event.target.value);
+        // alert(maxValue);
+        answer.textContent = "Найбільше число, яке ви ввели - " + String(maxValue);
+    });
+
+    // console.log(toString(i) + inputValues[i]);
+}
+
+
+
+
+// const inputElement = document.createElement("li");
+// inputList.appendChild(inputElement);
+
+// const input = document.createElement("input");
+
+// input.type = 'number';
+// input.placeholder = "Введіть число";
+// inputElement.appendChild(input);
+
+
+
+
+// answer
+
+// inputValues[1].value = 1;
+// answer.textContent = "Найбільше число, яке ви ввели - " + String(maxValue);
+
