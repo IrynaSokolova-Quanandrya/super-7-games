b6b58e68752b37fb4385b130c02bae9d46f97243
Iryna <irasokolova25@ukr.net>
2023-05-13 12:18:44 +0300

    змінює назви

--- src/index.js
+++ src/index.js
@@ -3,11 +3,8 @@ import './js/page/greet-modal';
 import './js/page/main-section';
 import './js/page/footer';
 
-
-
-
+// Ігри
 import './js/page/games/calculator';
-
 import './js/page/games/rock-paper-scissors';
 
 
