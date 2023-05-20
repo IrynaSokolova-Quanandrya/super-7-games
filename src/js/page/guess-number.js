abaadeaf6431dee5c11dd1f2b83b204d216ae33e
anastasiiabuniakina <109847740+anastasiiabuniakina@users.noreply.github.com>
2023-05-20 11:03:21 +0300

    edited

--- src/js/page/guess-number.js
+++ src/js/page/guess-number.js
@@ -6,17 +6,17 @@ let button2 = document.createElement('button')
 button2.setAttribute('type')
 button2.classList.add('button-2')
 
-let text2 = document.createElement('h3')
-text2.classList.add('h3-2')
-text2.textContent = 'Вгадай число, яке загадав компʼютер'
+// let text2 = document.createElement('h3')
+// text2.classList.add('h3-2')
+// text2.textContent = 'Вгадай число, яке загадав компʼютер'
 
 let result2 = document.createElement('p')
 result2.classList.add('p-2')
 result2.textContent = ''
 
-let body = document.getElementById('#2')
+let body = document.getElementById('2')
 
-body.appendChild(text2);
+// body.appendChild(text2);
 body.appendChild(input2);
 body.appendChild(button2);
 body.appendChild(result2);
--- src/js/page/leap-calculator.js
+++ src/js/page/leap-calculator.js
@@ -6,17 +6,17 @@ let button = document.createElement('button')
 button.setAttribute('type','button')
 button.classList.add('button-1')
 
-let text = document.createElement('h3')
-text.classList.add('h3')
-text.textContent = 'Перевір в який рік ти народився'
+// let text = document.createElement('h3')
+// text.classList.add('h3')
+// text.textContent = 'Перевір в який рік ти народився'
 
 let result = document.createElement('p')
 result.classList.add('p-1')
 result.textContent = ''
 
-let body = document.getElementById('#1')
+let body = document.getElementById('1')
 
-body.appendChild(text);
+// body.appendChild(text);
 body.appendChild(input);
 body.appendChild(button);
 body.appendChild(result);
