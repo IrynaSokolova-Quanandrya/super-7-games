f154abaca64ea344eabae3e273ce93acf1670757
Настя Скрицька <snastia.m@gmail.com>
2023-05-25 20:46:18 +0300

    new

--- src/js/page/header.js
+++ src/js/page/header.js
@@ -1,12 +1,13 @@
 const checkboxEl = document.querySelector(".topic-website-svg")
 const containerEl = document.querySelector(".container")
+const lightEl = document.querySelector(".light")
+const darkEl = document.querySelector(".dark")
 
 checkboxEl.addEventListener("change", changeTheme)
 
 function changeTheme(){
     if (checkboxEl.checked) {
         containerEl.classList.toggle("black-theme")
-        // console.log("clicked")
     } else{
         containerEl.classList.toggle("black-theme")
     }
