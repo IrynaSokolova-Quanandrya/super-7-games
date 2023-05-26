5039c02cb82a1294070cb3eaed01cea9f02b77ce
Daniil <negativdaniil4@gmail.com>
2023-05-26 18:14:31 +0300

    time.calculator

--- src/js/page/games/time-calc.js
+++ src/js/page/games/time-calc.js
@@ -1,3 +1,8 @@
+const gameConteiner = document.querySelector("#5")
+
+const markup = createElements();
+
+gameConteiner.innerHTML = markup;
 const input = document.querySelector(".wrapper-input > input");
 const time = document.querySelector("h4");
 const inputValue = input.value;
@@ -12,4 +17,29 @@ function calculator(event) {
    time.textContent = output;
 
 }
+
+function createElements() {
+   return `<section class="time-calculator">
+   <div class="container">
+      <div class="wrapper-title">
+         <div class="title">
+            <h2>Калькулятор часу</h2>
+         </div>
+      </div>
+      <div class="wrapper-content">
+         <div class="wrapper-input">
+            <input type="text" placeholder="Введіть число">
+            <div class="wrapper-svg">
+               <img src="/src/images/Group 104.svg" alt="">
+            </div>
+         </div>
+         <div class="wrapper-time">
+            <h4>00:00</h4>
+         </div>
+      </div>
+   </div>
+</section>`
+}
+
+
 
