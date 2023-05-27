ad71b2d9a798b91e7a3cfed822491a0cf087c075
Vlnduwfsfrekves <112093090+Vlnduwfsfrekves@users.noreply.github.com>
2023-05-27 13:04:26 +0300

    main-new-7

--- src/js/page/main-section.js
+++ src/js/page/main-section.js
@@ -1,8 +1,4 @@
 import games from './data/games.js';
-console.log(games);
-
 const list = document.querySelector('.class-list')
-
-const listItems = games.map(game => `<li id='${game.id}'><h2>${game.name}</h2></li>`);
-
+const listItems = games.map(game => `<li class="li-main" id='${game.id}'><svg class="img-main" xmlns="http://www.w3.org/2000/svg" width="536" height="2" viewBox="0 0 536 2"><path d="M0 1 H536" stroke="black" /></svg><h2 class='h2-main'>${game.name}</h2></li>`);
 list.innerHTML = listItems.join('')
