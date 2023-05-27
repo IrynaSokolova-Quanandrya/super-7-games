c9c79bbe3def86bb0ba3a37f2e021cc68fe3ef78
Настя Скрицька <snastia.m@gmail.com>
2023-05-27 11:27:53 +0300

    футер

--- src/index.js
+++ src/index.js
@@ -10,10 +10,10 @@ import './js/page/footer';
 import './js/page/leap-calculator';
 import './js/page/guess-number';
 import './js/page/games/rock-paper-scissors';
-import './js/page/games/calculator';
-import './js/page/games/time-calculator';
-import './js/page/games/dino/dino'
-import './js/page/games/football';
+// import './js/page/games/calculator';
+// import './js/page/games/time-calculator';
+// import './js/page/games/dino/dino'
+// import './js/page/games/football';
 import './js/page/data/our-team';
 import './js/page/games/scientist';
 
