94fdeaf64ad2f82b92c2540a5d42ab666cff6e97
Vlnduwfsfrekves <112093090+Vlnduwfsfrekves@users.noreply.github.com>
2023-05-27 13:05:28 +0300

    Update main-section.js

--- src/js/page/main-section.js
+++ src/js/page/main-section.js
@@ -1,4 +1,8 @@
 import games from './data/games.js';
+<<<<<<< Updated upstream
+=======
+
+>>>>>>> Stashed changes
 const list = document.querySelector('.class-list')
 const listItems = games.map(game => `<li class="li-main" id='${game.id}'><svg class="img-main" xmlns="http://www.w3.org/2000/svg" width="536" height="2" viewBox="0 0 536 2"><path d="M0 1 H536" stroke="black" /></svg><h2 class='h2-main'>${game.name}</h2></li>`);
 list.innerHTML = listItems.join('')
