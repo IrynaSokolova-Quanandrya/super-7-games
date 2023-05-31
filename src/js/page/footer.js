77659b4c4f401748a024a10e2e660b79fc483e79
Настя Скрицька <snastia.m@gmail.com>
2023-05-31 19:12:16 +0300

    footer-modal

--- src/js/page/footer.js
+++ src/js/page/footer.js
@@ -2,7 +2,7 @@ const formEl = document.querySelector(".form-submit")
 const closeModalBtn = document.querySelector("[data-modal-close]")
 const closeBtn = document.querySelector(".close-btn")
 const modal = document.querySelector("[data-modal]")
-const backdrop = document.querySelector(".backdrop")
+const backdrop = document.querySelector(".footer-backdrop")
 const inputEl = document.querySelector(".foot-input")
 
 formEl.addEventListener("submit", onBtnSubmit)
@@ -13,18 +13,7 @@ function onBtnSubmit(event) {
     event.preventDefault()
     if(!event.target.email.value){
         alert("Введіть вашу електрону адресу!")
-    }
-    
-    backdrop.classList.toggle('is-hidden');
-}
-
-inputEl.addEventListener("click", onBtnInp)
-
-function onBtnInp(event){
-    event.preventDefault()
-    if (inputEl.textContent = "") {
-        backdrop.classList.remove('is-hidden');
-    } else if(inputEl.textContent = "@"){
+    } else{
         backdrop.classList.toggle('is-hidden');
     }
 }
