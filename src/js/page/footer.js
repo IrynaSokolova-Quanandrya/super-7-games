0bc8ffe689d414687f191d6c16d9891d9a22dde4
Настя Скрицька <snastia.m@gmail.com>
2023-05-31 18:00:51 +0300

    swwf

--- src/js/page/footer.js
+++ src/js/page/footer.js
@@ -13,3 +13,14 @@ function onBtnSubmit(event) {
     event.preventDefault()
     backdrop.classList.toggle('is-hidden');
 }
+
+inputEl.addEventListener("click", onBtnInp)
+
+function onBtnInp(event){
+    event.preventDefault()
+    if (inputEl.textContent = "") {
+        backdrop.classList.remove('is-hidden');
+    } else if(inputEl.textContent = ""){
+        backdrop.classList.toggle('is-hidden');
+    }
+}
