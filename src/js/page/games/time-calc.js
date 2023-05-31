73b27599d45f974533b45f259d8f77365f80f7bf
Daniil <negativdaniil4@gmail.com>
2023-05-31 20:51:05 +0300

    time_calculator

--- src/js/page/games/time-calc.js
+++ src/js/page/games/time-calc.js
@@ -1,45 +1,42 @@
-const gameConteiner = document.getElementById("5")
-
+const gameContainer = document.getElementById("5");
 const markup = createElements();
+gameContainer.innerHTML = markup;
 
-gameConteiner.innerHTML = markup;
 const input = document.querySelector(".wrapper-input > input");
 const time = document.querySelector("h4");
-const inputValue = input.value;
 
 input.addEventListener("input", calculator);
+
 function calculator(event) {
+   const inputValue = parseInt(event.currentTarget.value);
 
-   const hours = Math.floor(event.currentTarget.value / 60);
-   const remainingMinutes = event.currentTarget.value % 60;
+   const days = Math.floor(inputValue / (60 * 24));
+   const hours = Math.floor((inputValue % (60 * 24)) / 60);
+   const minutes = inputValue % 60;
 
-   const output = `${hours.toString().padStart(2, '0')}:${remainingMinutes.toString().padStart(2, '0')}`;
+   const output = `${days}дн ${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}`;
    time.textContent = output;
-
 }
 
 function createElements() {
    return `<section class="time-calculator">
-   <div class="container">
-      <div class="wrapper-title">
-         <div class="title">
-            <h2>Калькулятор часу</h2>
-         </div>
-      </div>
-      <div class="wrapper-content">
-         <div class="wrapper-input">
-            <input type="text" placeholder="Введіть число">
-            <div class="wrapper-svg">
-               <img src="/src/images/Group 104.svg" alt="">
-            </div>
-         </div>
-         <div class="wrapper-time">
-            <h4>00:00</h4>
-         </div>
-      </div>
-   </div>
-</section>`
+    <div class="container">
+       <div class="wrapper-title">
+          <div class="title">
+             <h2>Калькулятор часу</h2>
+          </div>
+       </div>
+       <div class="wrapper-content">
+          <div class="wrapper-input">
+             <input type="text" placeholder="Введіть число">
+             <div class="wrapper-svg">
+                <img src="/src/images/Group 104.svg" alt="">
+             </div>
+          </div>
+          <div class="wrapper-time">
+             <h4>0дн 00:00</h4>
+          </div>
+       </div>
+    </div>
+ </section>`;
 }
-
-
-
