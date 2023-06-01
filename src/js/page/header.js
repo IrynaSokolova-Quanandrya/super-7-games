5f6ba64bd3acb81a48ee782a21dee6ac04dd0467
Iryna <irasokolova25@ukr.net>
2023-06-01 16:36:57 +0300

    fix theme

--- src/js/page/header.js
+++ src/js/page/header.js
@@ -1,6 +1,6 @@
 
 const checkboxEl = document.querySelector(".theme-switch__toggle")
-const containerEl = document.querySelector(".container")
+const containerEl = document.querySelectorAll(".container")
 const lightEl = document.querySelector(".light")
 const darkEl = document.querySelector(".dark")
 
@@ -10,9 +10,10 @@ checkboxEl.addEventListener("change", changeTheme)
 
 function changeTheme(){
     if (checkboxEl.checked) {
-        document.body.classList.toggle("black-theme")
+        containerEl.forEach((container)=>container.classList.toggle("black-theme"))
+        
     } else{
-        document.body.classList.toggle("black-theme")
+        containerEl.forEach((container)=>container.classList.toggle("black-theme"))
     }
 }
 
